dfcc3d7a6c14613b87a9a2987b7607a65c02d4d2
Ricardo Fernandes <ricardo.b.fernandes@accenture.com>
2026-09-23 14:57:40 -0300

    First Commit

---
+++ src/main/java/com/ricardo/PoCLibrary/PoCLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.PoCLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoCLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoCLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/PoCLibrary/PoCLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.PoCLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoCLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
